8436ce4071c05cbdfcfbce2844262c70b88f5cfc
Mateo Ferreira <matteo948@icloud.com>
2021-02-16 16:58:24 -0500

    sql seeds and schema

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ("Bacon Chesse",2)
+INSERT INTO burgers (burger_name,devoured) VALUES ("pickle Burger",3)
+INSERT INTO burgers (burger_name,devoured) VALUES ("chesse Burger ",5)
