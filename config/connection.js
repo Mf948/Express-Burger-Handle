2079f5d05cdfca40b5c240f44b1196be7d738ed3
Mateo Ferreira <matteo948@icloud.com>
2021-02-17 22:13:29 -0500

    .env

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
+require("dotenv").config();
+let password = process.env.password
+
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
   // NOTE: Be sure to add your MySQL password here!
-  password: 'Xferreira948',
+  password: `${password}`,
   database: 'burger_db',
 });
 
